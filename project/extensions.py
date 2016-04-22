77e2f7d60c172bca9ff578af64790533e00e3be1
VooDooBlooD <mdan.hagh@gmail.com>
2016-04-22 15:03:09 +0430

    add database extension

--- project/extensions.py
+++ project/extensions.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from flask.ext.cache import Cache
+from flask.ext.sqlalchemy import SQLAlchemy
 
-__all__ = ['cache']
+__all__ = ['cache', 'db']
 
 cache = Cache()
+db = SQLAlchemy()
