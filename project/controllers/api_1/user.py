cd089a640b5dd62bc856e3ee8e629bf88b051ff7
VooDooBlooD <mdan.hagh@gmail.com>
2016-04-22 20:37:53 +0430

    add docs

--- project/controllers/api_1/user.py
+++ project/controllers/api_1/user.py
@@ -16,16 +16,95 @@ from sqlalchemy.exc import IntegrityError
 
 
 
+###############################  View  ######################################
+#############################################################################
+
+
 @app.api_route('/user/', methods=['GET'])
 @login_required
 def get_user_info():
+	"""
+    Get Current User Info
+    Get basic information of current user
+    ---
+    tags:
+      - user
+    parameters:
+      - name: TOKEN
+        in: header
+        type: string
+        required: true
+        description: Token of current user
+    responses:
+      200:
+        description: Current user basic information
+        schema:
+          type: object
+          properties:
+            email:
+              type: string
+              description: Email of current user
+            firstname:
+              type: string
+              description: Firstname of current user
+            lastname:
+              type: string
+              description: Lastname of current user
+      401:
+        description: Token not found or is invalid or has expired
+	"""
+
 	user_obj = User.query.get(g.token_data['id'])
 	return jsonify(user_obj.to_json()), 200
+
+
+###############################  Login  #####################################
+#############################################################################
 
 
 @app.api_route('/user/login/', methods=['POST'])
 @api_validate_schema('user.login_schema')
 def login():
+	"""
+    Login
+    ---
+    tags:
+      - user
+    parameters:
+      - name: body
+        in: body
+        type: object
+        description: username and password for login
+        required: true
+        schema:
+          id: Login
+          required:
+            - username
+            - password
+          properties:
+            username:
+              type: string
+              example: babyknight
+            password:
+              type: string
+              example: baby123
+    responses:
+      200:
+        description: Successfully logged in
+        schema:
+          type: object
+          properties:
+            token:
+              type: string
+              description: Generated token for user
+      400:
+      	description: Bad request
+      404:
+        description: User does not exist
+      406:
+      	description: Wrong password
+	"""
+
 	data = request.json
 	username = data['username']
 	password = data['password']
@@ -38,13 +117,59 @@ def login():
 	return jsonify(errors='wrong password'), 406
 
 
+###############################  Signup  ####################################
+#############################################################################
+
+
 @app.api_route('/user/signup/', methods=['POST'])
 @api_validate_schema('user.signup_schema')
 def signup():
+	"""
+    Signup
+    ---
+    tags:
+      - user
+    parameters:
+      - name: body
+        in: body
+        type: object
+        description: username, email and password for signup
+        required: true
+        schema:
+          id: Signup
+          required:
+            - username
+            - email
+            - password
+          properties:
+            username:
+              type: string
+              pattern: ^[\w.]+$
+              example: babyknight
+              maxLength: 32
+            email:
+              type: string
+              example: baby@knight.org
+            password:
+              type: string
+              example: baby123
+              maxLength: 32
+    responses:
+      201:
+        description: Successfully registered
+      400:
+      	description: Bad request
+      406:
+      	description: Username or email already exist
+	"""
+
 	data = request.json
 	username = data['username']
 	email = data['email']
 	password = data['password']
+
+	if User.query.filter_by(email=email).first():
+		return jsonify(errors='email already exist'), 406
 
 	try:
 		user_obj = User(username=username, email=email)
@@ -55,3 +180,4 @@ def signup():
 		return jsonify(errors='username already exist'), 406
 
 	return '', 201
+
