3980b338699a43a1dc711c49e1b800390500e027
VooDooBlooD <mdan.hagh@gmail.com>
2016-04-24 22:50:43 +0430

    add edit_schema and min length for passwords

--- project/schemas/api_1/user.py
+++ project/schemas/api_1/user.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from good import Schema, All, Required, Length, Match, Email
+from good import Schema, All, Required, Optional, Length, Match, Email
 
 
 login_schema = Schema({
@@ -12,5 +12,15 @@ login_schema = Schema({
 signup_schema = Schema({
 						Required('username'): All(unicode, Match(r'^[\w.]+$'), Length(max=32)),
 						Required('email'): Email(),
-						Required('password'): All(unicode, Length(max=32))
+						Required('password'): All(unicode, Length(min=3, max=32))
+					   })
+
+
+edit_schema = Schema({
+						Optional('firstname'): All(unicode, Length(max=32)),
+						Optional('lastname'): All(unicode, Length(max=32)),
+						Optional('password'): Schema({
+														Required('old'): All(unicode, Length(min=3, max=32)),
+														Required('new'): All(unicode, Length(min=3, max=32))
+													 })
 					   })
