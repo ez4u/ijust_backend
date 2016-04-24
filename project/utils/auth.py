9610396fa744144a88545f715b42ae3b3c0b3c88
VooDooBlooD <mdan.hagh@gmail.com>
2016-04-24 20:48:19 +0430

    add encode and decode functions

--- project/utils/auth.py
+++ project/utils/auth.py
@@ -2,6 +2,7 @@
 
 # python imports
 from functools import wraps
+import json
 
 # flask imports
 from flask import request, jsonify, g
@@ -15,9 +16,16 @@ from project import app
 redis = Redis()
 
 
+def encode_data(data):
+	return json.dumps(data)
+
+def decode_data(encoded):
+	return json.loads(encoded)
+
+
 def generate_token(data):
 	token = str(uuid4())
-	redis.setex(token, data, app.config['TOKEN_EXPIRE_TIME'])
+	redis.setex(token, encode_data(data), app.config['TOKEN_EXPIRE_TIME'])
 	return token
 
 
@@ -37,7 +45,7 @@ def login_required(f):
 		if not token_data:
 			return jsonify(errors='token is invalid or has expired'), 401
 		
-		g.token_data = token_data
+		g.token_data = decode_data(token_data)
 
 		return f(*args, **kwargs)
 
